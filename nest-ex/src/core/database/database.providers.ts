0af614b8cf1eba586034914c5e9d90f0320d7d42
annpuzu <annpuzu@gmail.com>
2026-09-11 07:26:12 +0300

    nest-ex: NestJS auth module (JWT + local strategy)

---
+++ nest-ex/src/core/database/database.providers.ts
@@ -0,0 +1,31 @@
+import { Sequelize } from 'sequelize-typescript';
+import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
+import { databaseConfig } from './database.config';
+import { User } from '../../modules/users/user.entity';
+
+export const databaseProviders = [
+  {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    provide: SEQUELIZE,
+    useFactory: async () => {
+      let config;
+      switch (process.env.NODE_ENV) {
+        case DEVELOPMENT:
+          config = databaseConfig.development;
+          break;
+        case TEST:
+          config = databaseConfig.test;
+          break;
+        case PRODUCTION:
+          config = databaseConfig.production;
+          break;
+        default:
+          config = databaseConfig.development;
+      }
+      const sequelize = new Sequelize(config);
+      sequelize.addModels([User]);
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];
